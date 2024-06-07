03fb20d00cadc7fa6e7a75499570db2a043314f5
samuel-wambui <147292274+samuel-wambui@users.noreply.github.com>
2024-06-07 11:13:38 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,32 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int arry[]={1,2,3,4,5,6,7,8,9};
+        int target=7;
+        int result=binarysearch(arry,target);
+        if(result !=-1){
+            System.out.println("number found at index " +result);
         }
+        else System.out.println("number not found");
+
     }
-}
+
+    private static int binarysearch(int[] arry, int target) {
+        int start=0;
+        int end= arry.length;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arry[mid]==target){
+                return mid;
+            } else if (arry[mid]<target) {
+                start=mid+1;
+            } else if (arry[mid]>target) {
+                end=mid-1;
+            }
+            else {
+                return 0;
+            }
+        }
+        return -1;
+    }
+    }
